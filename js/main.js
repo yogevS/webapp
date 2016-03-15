4f8faee11e76500323f401cf216b6259eab50bd1
yogevS <yogev338@walla.com>
2016-03-15 23:01:45 +0200

    js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 function submitReports(){
 var error="";
-
+var data='{"tab":1,' ;
   var ddlist=document.getElementById("report1Select");//dropdwon list
   var option=ddlist.children;
 
@@ -60,10 +60,10 @@ else{
   option[0].value=url;
   document.getElementById("report1Iframe").src = url;//put frame
   document.getElementById("report1NTL").href = url;//put new tabl link
-
+  data=data + '"url1":' + '"' + url + '"' + ',"name1":' + '"' + name + '"';
 
   if((url2 != null && url2 != "" )&&( name2 != null && name2 != "")){//cheking if report 2 exist
-
+  data=data + ',"url2":' + '"' + url2 + '"' + ',"name2":' + '"' + name2 + '"';
     if(ddlist.length<2){//need to crate option
      var optionCRT2 = document.createElement("OPTION");        // Create a <button> element
      var t2 = document.createTextNode(name2);       // Create a text node
@@ -86,6 +86,7 @@ else{
 
 
 if((url3 != null && url3 != "" )&&( name3 != null && name3 != "")){//cheking if report 3 exist
+data=data + ',"url3":' + '"' + url3 + '"' + ',"name3":' + '"' + name3 + '"';
 if(ddlist.length<3){//need to crate option
   var optionCRT = document.createElement("OPTION");        // Create a <button> element
   var  t = document.createTextNode(name3);       // Create a text node
@@ -98,6 +99,7 @@ else{
   option[2].value=url3;
 }
   }
+
 else{//report 3 doesnt exist
 if(ddlist.length==3){
 ddlist.removeChild(ddlist.childNodes[3]);
@@ -112,6 +114,14 @@ var i;
 for(i=0 ; i<length ;i++){
   x[i].style.display="block";
 }
+
+// Save data as a JSON string
+
+data = data + "}";
+
+localStorage.setItem('data', JSON.stringify(data));
+
+// Get the data out as a JSON object
 
   document.getElementById("error").innerHTML="";//dropdwon list
 
@@ -132,6 +142,7 @@ return false;
 
 function submitSearch(){
 getNotifications();//restart the notifications
+
   var val = document.forms["searchForm"]["q"].value;//get the url
   var ddlist=document.getElementById("report1Select");//dropdwon list
   var option=ddlist.children;
@@ -225,9 +236,51 @@ UTILS.ajax("data/config.json",notifications);
 
 /** ***********************************    **/
 
+function pageLoad(){
+
+ var myData=JSON.parse(JSON.parse(localStorage.getItem('data')));//wtf why i need 2 Jason.prase
+ getNotifications();
+ switch(myData.tab){
+ case 1:
+ location.hash="#quick-reports";
+ tab1On();
+ break;
+ case 2:
+ location.hash="#my-folders";
+ tab2On();
+ break;
+ case 3:
+ location.hash="#my-team-folders";
+ tab3On();
+ break;
+ case 4:
+ location.hash="#public-folders";
+ tab4On();
+ break;
+}//end of switch
+
+ document.forms["report1Form"]["webSiteURL1"].value=myData.url1;//set the url in form
+document.forms["report1Form"]["name1"].value=myData.name1;//set the name in form
+if(myData.url2!=null){//check if we intzallize this report
+document.forms["report1Form"]["webSiteURL2"].value=myData.url2;//set the url in form
+}
+if(myData.name2!=null){
+document.forms["report1Form"]["name2"].value=myData.name2;//set the name in form
+}
+if(myData.url3!=null){//check if we intzallize this report
+document.forms["report1Form"]["webSiteURL3"].value=myData.url3;//set the url in form
+}
+if(myData.name3!=null){
+document.forms["report1Form"]["name3"].value=myData.name3;//set the name in form
+}
+
+submitReports();//send the value in the form we put
+
+}//end of pageLoad
+
+/** ***********************************    **/
 
 function getNotifications(){
-
   var xhttp = new XMLHttpRequest();
   xhttp.open("GET", "data/config.json");
     xhttp.onreadystatechange = function() {
@@ -235,6 +288,7 @@ function getNotifications(){
         document.getElementById("notificationsCon").innerHTML  = status;
       if (xhttp.readyState == 4 && ((status >= 200 && status < 300) || status === 304)) {
          document.getElementById("notificationsCon").innerHTML  = xhttp.responseText;
+
       }
     };
 
@@ -243,13 +297,19 @@ function getNotifications(){
 
 /** ***********************************    **/
 
-document.getElementById("tabHead1").addEventListener("click", function(){
+document.getElementById("tabHead1Con").addEventListener("click", function(){
 
 tab1On();
 
 });
 
 function tab1On(){
+
+  var prefix='{"tab":1'
+  var myData=JSON.parse(localStorage.getItem('data'));//wtf why i need 2 Jason.prase
+  myData= '{"tab":1' + myData.substr(prefix.length,myData.length);
+  localStorage.setItem('data',JSON.stringify(myData));
+
   document.getElementById("tabHead1").style.background ="#d9d9d9";
   document.getElementById("tabHead1Con").style.color ="black";
 
@@ -265,55 +325,86 @@ function tab1On(){
 
 /** ***********************************    **/
 
-document.getElementById("tabHead2").addEventListener("click", function(){
+document.getElementById("tabHead2Con").addEventListener("click", function(){
 
-document.getElementById("tabHead2").style.background ="#d9d9d9";
-document.getElementById("tabHead2Con").style.color ="black";
-
-  document.getElementById("tabHead1").style.background ="#737373";
-  document.getElementById("tabHead1Con").style.color ="white";
-
-  document.getElementById("tabHead3").style.background ="#737373";
-  document.getElementById("tabHead3Con").style.color ="white";
-
-  document.getElementById("tabHead4").style.background ="#737373";
-  document.getElementById("tabHead4Con").style.color ="white";
+tab2On();
 
 });
+
+function tab2On(){
+
+var prefix='{"tab":1';
+var myData=JSON.parse(localStorage.getItem('data'));//wtf why i need 2 Jason.prase
+myData= '{"tab":2' + myData.substr(prefix.length,myData.length);
+localStorage.setItem('data',JSON.stringify(myData));
+
+  document.getElementById("tabHead2").style.background ="#d9d9d9";
+  document.getElementById("tabHead2Con").style.color ="black";
+
+    document.getElementById("tabHead1").style.background ="#737373";
+    document.getElementById("tabHead1Con").style.color ="white";
+
+    document.getElementById("tabHead3").style.background ="#737373";
+    document.getElementById("tabHead3Con").style.color ="white";
+
+    document.getElementById("tabHead4").style.background ="#737373";
+    document.getElementById("tabHead4Con").style.color ="white";
+
+}
 
 /** ***********************************    **/
 
-document.getElementById("tabHead3").addEventListener("click", function(){
+document.getElementById("tabHead3Con").addEventListener("click", function(){
 
-document.getElementById("tabHead3").style.background ="#d9d9d9";
-document.getElementById("tabHead3Con").style.color ="black";
-
-  document.getElementById("tabHead2").style.background ="#737373";
-  document.getElementById("tabHead2Con").style.color ="white";
-
-  document.getElementById("tabHead1").style.background ="#737373";
-  document.getElementById("tabHead1Con").style.color ="white";
-
-  document.getElementById("tabHead4").style.background ="#737373";
-  document.getElementById("tabHead4Con").style.color ="white";
+tab3On();
 
 });
 
+function tab3On(){
+
+  var prefix='{"tab":1';
+  var myData=JSON.parse(localStorage.getItem('data'));//wtf why i need 2 Jason.prase
+  myData= '{"tab":3' + myData.substr(prefix.length,myData.length);
+  localStorage.setItem('data',JSON.stringify(myData));
+
+
+  document.getElementById("tabHead3").style.background ="#d9d9d9";
+  document.getElementById("tabHead3Con").style.color ="black";
+
+    document.getElementById("tabHead2").style.background ="#737373";
+    document.getElementById("tabHead2Con").style.color ="white";
+
+    document.getElementById("tabHead1").style.background ="#737373";
+    document.getElementById("tabHead1Con").style.color ="white";
+
+    document.getElementById("tabHead4").style.background ="#737373";
+    document.getElementById("tabHead4Con").style.color ="white";
+
+}
 
 /** ***********************************    **/
 
-document.getElementById("tabHead4").addEventListener("click", function(){
-
-document.getElementById("tabHead4").style.background ="#d9d9d9";
-document.getElementById("tabHead4Con").style.color ="black";
-
-  document.getElementById("tabHead2").style.background ="#737373";
-  document.getElementById("tabHead2Con").style.color ="white";
-
-  document.getElementById("tabHead3").style.background ="#737373";
-  document.getElementById("tabHead3Con").style.color ="white";
-
-  document.getElementById("tabHead1").style.background ="#737373";
-  document.getElementById("tabHead1Con").style.color ="white";
-
+document.getElementById("tabHead4Con").addEventListener("click", function(){
+tab4On();
 });
+
+
+function tab4On(){
+
+var prefix='{"tab":1';//the data is in format when tab is first
+  var myData=JSON.parse(localStorage.getItem('data'));//wtf why i need 2 Jason.prase
+  myData= '{"tab":4' + myData.substr(prefix.length,myData.length);//change mydata
+  localStorage.setItem('data',JSON.stringify(myData));//save the data
+
+  document.getElementById("tabHead4").style.background ="#d9d9d9";
+  document.getElementById("tabHead4Con").style.color ="black";
+
+    document.getElementById("tabHead2").style.background ="#737373";
+    document.getElementById("tabHead2Con").style.color ="white";
+
+    document.getElementById("tabHead3").style.background ="#737373";
+    document.getElementById("tabHead3Con").style.color ="white";
+
+    document.getElementById("tabHead1").style.background ="#737373";
+    document.getElementById("tabHead1Con").style.color ="white";
+}
