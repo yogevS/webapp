983f490399cbcd7bdd28b9325f9c7f4a6f55b5a2
yogevS <yogev338@walla.com>
2016-03-17 21:03:11 +0200

    adding my-team-reports

--- js/main.js
+++ js/main.js
@@ -1,69 +1,122 @@
 
-function submitReports(){
+function submitReports(i){//getting the report who active this
 var error="";
-var data='{"tab":1,' ;
-  var ddlist=document.getElementById("report1Select");//dropdwon list
+var b=false;//bollean var in case when we reomve 2 and 3 toghter
+
+
+if(localStorage.getItem('data')!=null){
+var data=JSON.parse(localStorage.getItem('data'));
+}
+
+else{  //if none storage then intillaize
+var data={
+tab:1,name1:"",url1:"",name2:"",url2:"",
+  name3:"",url3:"",name4:"",url4:""
+  ,name5:"",url5:"",name6:"",url6:""
+} ;
+
+}
+
+
+  var ddlist=document.getElementById("report" + i + "Select");//dropdwon list
   var option=ddlist.children;
 
-document.forms["report1Form"]["name2"].style.border="none";
-document.forms["report1Form"]["webSiteURL2"].style.border="none";
-document.forms["report1Form"]["name3"].style.border="none";
-document.forms["report1Form"]["webSiteURL3"].style.border="none";
+document.forms["report" + i + "Form"]["name2"].style.border="none";  //intillize for error
+document.forms["report" + i + "Form"]["webSiteURL2"].style.border="none";
+document.forms["report" + i + "Form"]["name3"].style.border="none";
+document.forms["report" + i + "Form"]["webSiteURL3"].style.border="none";
 
+document.getElementById("error" + i).innerHTML="";//intillize error
 
-  var url = document.forms["report1Form"]["webSiteURL1"].value;//get the url
-  var name=document.forms["report1Form"]["name1"].value;//get the name for url
+  var url = document.forms["report" + i + "Form"]["webSiteURL1"].value;//get the url
+  var name=document.forms["report" + i + "Form"]["name1"].value;//get the name for url
 
-  var url2 = document.forms["report1Form"]["webSiteURL2"].value;//get the url
-  var name2=document.forms["report1Form"]["name2"].value;//get the name for url
+  var url2 = document.forms["report" + i + "Form"]["webSiteURL2"].value;//get the url
+  var name2=document.forms["report" + i + "Form"]["name2"].value;//get the name for url
 
-  var url3 = document.forms["report1Form"]["webSiteURL3"].value;//get the url
-  var name3=document.forms["report1Form"]["name3"].value;//get the name for url
+  var url3 = document.forms["report" + i + "Form"]["webSiteURL3"].value;//get the url
+  var name3=document.forms["report" + i + "Form"]["name3"].value;//get the name for url
 
 if(((url2 == null || url2 == "" )||( name2 == null || name2 == ""))&&((url3 != null && url3 != "" )&&( name3 != null && name3 != ""))){
   //cheking format,3 doesnt exist before 2
   error="format is wrong,please keep the format!,fill 2 before 3.";
-  document.getElementById("error").innerHTML=error;//dropdwon list
+  document.getElementById("error" +i).innerHTML=error;//dropdwon list
 }
 
 
 else{
 
 if((url2 != null && url2 != "" )&&( name2 == null || name2 == "")){
-document.forms["report1Form"]["name2"].style.border="2px solid red";
-document.forms["report1Form"]["name2"].focus();
+document.forms["report" + i + "Form"]["name2"].style.border="2px solid red";
+document.forms["report" + i + "Form"]["name2"].focus();
 }
 
 else{
 
    if((name2 != null && name2 != "" )&&( url2 == null || url2 == "")){
- document.forms["report1Form"]["webSiteURL2"].style.border="2px solid red";
-   document.forms["report1Form"]["webSiteURL2"].focus();
+ document.forms["report" + i + "Form"]["webSiteURL2"].style.border="2px solid red";
+   document.forms["report" + i + "Form"]["webSiteURL2"].focus();
    }
   else{
 
     if((url3 != null && url3 != "" )&&( name3 == null || name3 == "")){
-    document.forms["report1Form"]["name3"].style.border="2px solid red";
-    document.forms["report1Form"]["name3"].focus();
+    document.forms["report" + i + "Form"]["name3"].style.border="2px solid red";
+    document.forms["report" + i + "Form"]["name3"].focus();
     }
 
     else{
 
       if((name3 != null && name3 != "" )&&( url3 == null || url3 == "")){
-    document.forms["report1Form"]["webSiteURL3"].style.border="2px solid red";
-      document.forms["report1Form"]["webSiteURL3"].focus();
+    document.forms["report" + i + "Form"]["webSiteURL3"].style.border="2px solid red";
+      document.forms["report" + i + "Form"]["webSiteURL3"].focus();
       }
      else{
 
 
   option[0].innerHTML=name;
   option[0].value=url;
-  document.getElementById("report1Iframe").src = url;//put frame
-  document.getElementById("report1NTL").href = url;//put new tabl link
-  data=data + '"url1":' + '"' + url + '"' + ',"name1":' + '"' + name + '"';
+  document.getElementById("report" + i + "Iframe").src = url;//put frame
+  document.getElementById("report" + i + "NTL").href = url;//put new tabl link
+
+switch(i){
+case 1:
+
+data.url1=url;
+data.name1=name;
+
+break;
+
+case 2:
+
+data.url4=url;
+data.name4=name;
+
+
+break;
+}
+
+
 
   if((url2 != null && url2 != "" )&&( name2 != null && name2 != "")){//cheking if report 2 exist
-  data=data + ',"url2":' + '"' + url2 + '"' + ',"name2":' + '"' + name2 + '"';
+
+    switch(i){
+    case 1:
+
+    data.url2=url2;
+    data.name2=name2;
+
+    break;
+
+    case 2:
+
+    data.url5=url2;
+    data.name5=name2;
+
+
+    break;
+    }
+
+
     if(ddlist.length<2){//need to crate option
      var optionCRT2 = document.createElement("OPTION");        // Create a <button> element
      var t2 = document.createTextNode(name2);       // Create a text node
@@ -77,53 +130,108 @@ else{
      }
 
   }
+
  else{//report 2 doesnt exist
-    if(ddlist.length==2){
-    ddlist.removeChild(ddlist.childNodes[2]);
+
+   switch(i){
+   case 1:
+
+   data.url2="";
+   data.name2="";
+
+   break;
+
+   case 2:
+
+   data.url5="";
+   data.name5="";
+
+
+   break;
+   }
+
+    if(ddlist.length==2||ddlist.length==3){
+      b=true;//only in case when we remove 2 before 3
+    ddlist.removeChild(option[1]);
      }
   }
 
 
 
 if((url3 != null && url3 != "" )&&( name3 != null && name3 != "")){//cheking if report 3 exist
-data=data + ',"url3":' + '"' + url3 + '"' + ',"name3":' + '"' + name3 + '"';
+
+  switch(i){
+  case 1:
+
+  data.url3=url3;
+  data.name3=name3;
+
+  break;
+
+  case 2:
+
+  data.url6=url3;
+  data.name6=name3;
+
+
+  break;
+  }
+
 if(ddlist.length<3){//need to crate option
   var optionCRT = document.createElement("OPTION");        // Create a <button> element
   var  t = document.createTextNode(name3);       // Create a text node
   optionCRT.value=url3;
   optionCRT.appendChild(t);                                // Append the text to <button>
   ddlist.appendChild(optionCRT);                    // Append <button> to <body>
-}
-else{
-  option[2].innerHTML=name3;
-  option[2].value=url3;
-}
   }
+ else{
+   option[2].innerHTML=name3;
+   option[2].value=url3;
+   }
+}
+
 
 else{//report 3 doesnt exist
+  switch(i){
+  case 1:
+
+  data.url3="";
+  data.name3="";
+
+  break;
+
+  case 2:
+
+  data.url6="";
+  data.name6="";
+
+
+  break;
+  }
+
 if(ddlist.length==3){
-ddlist.removeChild(ddlist.childNodes[3]);
+ddlist.removeChild(option[2]);
 }
+if(b && ddlist.length==2){//when we remove 2 before 3,length is 2 so we wont catch it
+ddlist.removeChild(option[1]);
+}
+
 }
 
 
-document.getElementById("report1CB").checked = false;//hiding setting
-var x=document.querySelectorAll(".hidden");//showing elements we hidden
+document.getElementById("report" + i + "CB").checked = false;//hiding setting
+
+var x=document.querySelectorAll(".hidden" + i);//showing elements we hidden
 var length=x.length;
-var i;
-for(i=0 ; i<length ;i++){
-  x[i].style.display="block";
+var j;
+for(j=0 ; j<length ;j++){
+  x[j].style.display="block";
 }
 
-// Save data as a JSON string
-
-data = data + "}";
 
 localStorage.setItem('data', JSON.stringify(data));
 
 // Get the data out as a JSON object
-
-  document.getElementById("error").innerHTML="";//dropdwon list
 
 
 }//else for if url3 missing
@@ -149,16 +257,36 @@ function submitSearch(){
   var b=true;
   var i=0;
   while( i<length && b){
-    if(option[i].innerHTML.search(val)>=0){
+    if(option[i].innerHTML.search(val)>=0){//meaning we found a report
      tab1On();
       location.hash="#quick-reports";
       ddlist.selectedIndex =i;
-      changeTab();//change the tab
+      changeTab(1);//change the tab
         b=false;
         getNotifications();//restart the notifications
     }
+
     i++;
   }
+
+   i=0;//now we search on the my-team-folders
+   var ddlist=document.getElementById("report2Select");//dropdwon list
+   var option=ddlist.children;
+
+   while( i<length && b){
+     if(option[i].innerHTML.search(val)>=0){//meaning we found a report
+      tab3On();
+       location.hash="#my-team-folders";
+       ddlist.selectedIndex =i;
+       changeTab(2);//change the tab
+         b=false;
+         getNotifications();//restart the notifications
+     }
+
+     i++;
+   }
+
+
   if(b){//meaning we didnt find nothing
    document.getElementById("notificationsCon").innerHTML ="The searched report <b>" + val + "</b> was not found.";
   }
@@ -169,18 +297,18 @@ function submitSearch(){
 
 /** ***********************************    **/
 
-function changeTab(){
-var ddlist=document.getElementById("report1Select");//dropdwon list
-  var sel=ddlist.value;
+function changeTab(i){//i is 1 or 2,for quick reports and my-team-folders tabs..
 
-  document.getElementById("report1Iframe").src = sel;//put frame
-  document.getElementById("report1NTL").href = sel;//put new tabl link
+var ddlist=document.getElementById("report" + i + "Select");//dropdwon list
+  var sel=ddlist.value;
+  document.getElementById("report" + i + "Iframe").src = sel;//put frame
+  document.getElementById("report" + i + "NTL").href = sel;//put new tabl link
 }
 
 /** ***********************************    **/
 
 
-document.getElementById("tabSettingIm").addEventListener("click", function(){//wheel roatate
+document.getElementById("SettingImBox").addEventListener("click", function(){//wheel roatate for form 1
 var t=document.getElementById("tabSettingIm");
 if(t.style.transform == ""){
 t.style.transform="rotate(360deg)"
@@ -191,15 +319,30 @@ else{
 
 });
 
+
+document.getElementById("SettingImBox2").addEventListener("click", function(){//wheel roatate for form 2
+var t=document.getElementById("tabSettingIm2");
+if(t.style.transform == ""){
+t.style.transform="rotate(360deg)"
+}
+else{
+  t.style.transform=""
+}
+
+});
+
+
+
 /** ***********************************    **/
 
-function identifyEscKeyPressedEvent(keyEvent){
+function identifyEscKeyPressedEvent(keyEvent,Id){
             var pressedKeyValue = keyEvent.keyCode || keyEvent.which;
             if(pressedKeyValue == 27)
             {
-            document.getElementById("report1CB").checked = false;//hidden report form
+            document.getElementById(Id).checked = false;//hidden report form
             }
     }
+
 
 /** ***********************************    **/
 
@@ -236,8 +379,8 @@ UTILS.ajax("data/config.json",notifications);
 
 function pageLoad(){
 
- var myData=JSON.parse(JSON.parse(localStorage.getItem('data')));//wtf why i need 2 Jason.prase
- getNotifications();
+ var myData=JSON.parse(localStorage.getItem('data'));//wtf why i need 2 Jason.prase
+getNotifications();
  switch(myData.tab){
  case 1:
  location.hash="#quick-reports";
@@ -259,6 +402,7 @@ function pageLoad(){
 
  document.forms["report1Form"]["webSiteURL1"].value=myData.url1;//set the url in form
 document.forms["report1Form"]["name1"].value=myData.name1;//set the name in form
+
 if(myData.url2!=null){//check if we intzallize this report
 document.forms["report1Form"]["webSiteURL2"].value=myData.url2;//set the url in form
 }
@@ -272,7 +416,26 @@ if(myData.name3!=null){
 document.forms["report1Form"]["name3"].value=myData.name3;//set the name in form
 }
 
-submitReports();//send the value in the form we put
+
+document.forms["report2Form"]["webSiteURL1"].value=myData.url4;//set the url in form
+document.forms["report2Form"]["name1"].value=myData.name4;//set the name in form
+
+if(myData.url5!=null){//check if we intzallize this report
+document.forms["report2Form"]["webSiteURL2"].value=myData.url5;//set the url in form
+}
+if(myData.name5!=null){
+document.forms["report2Form"]["name2"].value=myData.name5;//set the name in form
+}
+if(myData.url6!=null){//check if we intzallize this report
+document.forms["report2Form"]["webSiteURL3"].value=myData.url6;//set the url in form
+}
+if(myData.name6!=null){
+document.forms["report2Form"]["name3"].value=myData.name6;//set the name in form
+}
+
+
+submitReports(1);//send the value in the form we put
+submitReports(2);//send the value in the form we put
 
 }//end of pageLoad
 
@@ -307,9 +470,9 @@ tab1On();
 
 function tab1On(){
 
-  var prefix='{"tab":1'
-  var myData=JSON.parse(localStorage.getItem('data'));//wtf why i need 2 Jason.prase
-  myData= '{"tab":1' + myData.substr(prefix.length,myData.length);
+
+  var myData=JSON.parse(localStorage.getItem('data'));
+  myData.tab=1;
   localStorage.setItem('data',JSON.stringify(myData));
 
   document.getElementById("tabHead1").style.background ="#d9d9d9";
@@ -335,9 +498,8 @@ tab2On();
 
 function tab2On(){
 
-var prefix='{"tab":1';
-var myData=JSON.parse(localStorage.getItem('data'));//wtf why i need 2 Jason.prase
-myData= '{"tab":2' + myData.substr(prefix.length,myData.length);
+var myData=JSON.parse(localStorage.getItem('data'));
+myData.tab=2;
 localStorage.setItem('data',JSON.stringify(myData));
 
   document.getElementById("tabHead2").style.background ="#d9d9d9";
@@ -364,9 +526,8 @@ tab3On();
 
 function tab3On(){
 
-  var prefix='{"tab":1';
-  var myData=JSON.parse(localStorage.getItem('data'));//wtf why i need 2 Jason.prase
-  myData= '{"tab":3' + myData.substr(prefix.length,myData.length);
+  var myData=JSON.parse(localStorage.getItem('data'));
+  myData.tab=3;
   localStorage.setItem('data',JSON.stringify(myData));
 
 
@@ -393,10 +554,9 @@ tab4On();
 
 function tab4On(){
 
-var prefix='{"tab":1';//the data is in format when tab is first
-  var myData=JSON.parse(localStorage.getItem('data'));//wtf why i need 2 Jason.prase
-  myData= '{"tab":4' + myData.substr(prefix.length,myData.length);//change mydata
-  localStorage.setItem('data',JSON.stringify(myData));//save the data
+  var myData=JSON.parse(localStorage.getItem('data'));
+  myData.tab=4;
+  localStorage.setItem('data',JSON.stringify(myData));
 
   document.getElementById("tabHead4").style.background ="#d9d9d9";
   document.getElementById("tabHead4Con").style.color ="black";
